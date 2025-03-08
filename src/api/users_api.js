97abe7c54df5c77a9c3edb447006d965e31b32d6
Aasim-Qureshi <aasimq194@gmail.com>
2025-03-08 12:58:52 +0530

    Error fix

--- src/api/users_api.js
+++ src/api/users_api.js
@@ -1,5 +1,3 @@
-import axios from 'axios';
-
 const sendUserInfo = async (email, username, password) => {
     try {
         const response = await axios.post('https://get-it-done-backend.onrender.com/users/add-user', {
@@ -11,27 +9,39 @@ const sendUserInfo = async (email, username, password) => {
         console.log("Success: ", response.data);
         return response;
 
-    } catch(err) {
-        console.log("There was an error: ", err);
+    } catch (err) {
+        if (err.response) {
+            // Server responded with an error
+            console.log("Error Response Data: ", err.response.data);
+            console.log("Error Status: ", err.response.status);
+        } else {
+            // Something else went wrong
+            console.log("Error: ", err.message);
+        }
     }
-}
+};
 
 const sendLoginInfo = async (email, password) => {
     try {
         const response = await axios.post('https://get-it-done-backend.onrender.com/users/login', {
             email: email,
             password: password
-        },
-        {withCredentials: true}
-    );
+        }, { withCredentials: true });
 
         console.log("Success: ", response.data);
         return response;
 
-    } catch(err) {
-        console.log("There was an error: ", err);
+    } catch (err) {
+        if (err.response) {
+            // Server responded with an error
+            console.log("Error Response Data: ", err.response.data);
+            console.log("Error Status: ", err.response.status);
+        } else {
+            // Something else went wrong
+            console.log("Error: ", err.message);
+        }
         throw err;
     }
-}
+};
 
-export {sendUserInfo, sendLoginInfo};
+export { sendUserInfo, sendLoginInfo };
